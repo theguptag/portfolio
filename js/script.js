b0a276239d007dec9234655ea4600a7ed9072ef0
theguptag <ganpatgupta999@gmail.com>
2022-05-16 17:31:19 +0530

    some final changes
    - blog anchor tag
    - skills heading
    - dark mode icon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    if (element.classList.contains('dark-mode')) {
+        document.getElementById('dark-mode-img').style.opacity = 0;
+        document.getElementById('light-mode-img').style.opacity = 1;
+    } else {
+        document.getElementById('dark-mode-img').style.opacity = 1;
+        document.getElementById('light-mode-img').style.opacity = 0;
+    }
 }
 
